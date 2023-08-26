9014cb393a4804fac798ce3ba954d415b3c43f4e
SeungUk Ahn <ahn2802@gmail.com>
2023-08-26 17:22:00 +0900

    Before, After, AfterReturning, AfterThrowing aspect 적용

--- src/main/java/com/maicoding/learnspringaop/aopexample/aspects/LoggingAspect.java
+++ src/main/java/com/maicoding/learnspringaop/aopexample/aspects/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.maicoding.learnspringaop.aopexample.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +13,26 @@ public class LoggingAspect {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Before("execution(* com.maicoding.learnspringaop.aopexample.*.*.*(..))")
-    public void logMethodCall(JoinPoint joinPoint){
-        logger.info("Before Aspect - Method is called - {}", joinPoint);
+    public void logMethodCallBeforeExecution(JoinPoint joinPoint){
+        logger.info("Before Aspect - {} is called with args: {}", joinPoint, joinPoint.getArgs());
+    }
+
+    @After("execution(* com.maicoding.learnspringaop.aopexample.*.*.*(..))")
+    public void logMethodCallAfterExecution(JoinPoint joinPoint){
+        logger.info("After Aspect - {} is executed", joinPoint);
+    }
+
+    @AfterThrowing(
+        pointcut = "execution(* com.maicoding.learnspringaop.aopexample.*.*.*(..))",
+        throwing = "exception")
+    public void logMethodCallAfterException(JoinPoint joinPoint, Exception exception){
+        logger.info("AfterThrowing Aspect - {} has thrown an exception: {}", joinPoint, exception);
+    }
+
+    @AfterReturning(
+        pointcut = "execution(* com.maicoding.learnspringaop.aopexample.*.*.*(..))",
+        returning = "resultValue")
+    public void logMethodCallAfterSuccess(JoinPoint joinPoint, Object resultValue){
+        logger.info("AfterReturning Aspect - {} successfully returned {}", joinPoint, resultValue);
     }
 }
